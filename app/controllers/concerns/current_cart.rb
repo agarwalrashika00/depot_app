1a8c25f6a729b8b12cc0b2748172832d6c47998e
Rashika <agarwalrashika00@gmail.com>
2023-03-22 01:37:17 +0530

    added cart

---
+++ app/controllers/concerns/current_cart.rb
@@ -0,0 +1,9 @@
+module CurrentCart
+  private
+  def set_cart
+    @cart = Cart.find(session[:card_id])
+  rescue ActiveRecord::RecordNotFound
+    @cart = Cart.create
+    session[:card_id] = @cart.id
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  resources :line_items
+  resources :carts
+  # get 'store/index'
   resources :products
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "store#index", as: 'store_index'
   get "/products", to: "products#index"
 end
