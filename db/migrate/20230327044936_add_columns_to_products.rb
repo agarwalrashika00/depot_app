381a46f52be9fbffaabacfdc84c1dd22944e64a0
Rashika <agarwalrashika00@gmail.com>
2023-03-27 21:01:39 +0530

    basic structure of migration extension

--- db/migrate/20230327044936_add_columns_to_products.rb
+++
@@ -1,7 +0,0 @@
-class AddColumnsToProducts < ActiveRecord::Migration[7.0]
-  def change
-    add_column :products, :enabled, :boolean
-    add_column :products, :discount_price, :decimal
-    add_column :products, :permalink, :string
-  end
-end
--- db/migrate/20230327054225_add_line_items_count_to_cart.rb
+++
@@ -1,5 +0,0 @@
-class AddLineItemsCountToCart < ActiveRecord::Migration[7.0]
-  def change
-    add_column :carts, :line_items_count, :integer, null: false, default: 0
-  end
-end
