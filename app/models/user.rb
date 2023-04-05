a62b904aadf04403d047ead99a0379e00ebb5b7a
Rashika <agarwalrashika00@gmail.com>
2023-04-05 13:34:34 +0530

    changes saved to user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,16 +2,8 @@ class User < ApplicationRecord
   after_destroy :ensure_an_admin_remains
   validates :name, presence: true, uniqueness: true
   has_secure_password
-<<<<<<< HEAD
   validates :email, uniqueness: true, allow_blank: true, format: {
-=======
-  validates :email, uniqueness: true, format: {
-<<<<<<< HEAD
->>>>>>> 82f3780 (rebasing callback over validation)
     with: EMAIL_REGEXP
-=======
-    with: /\A[\w-]+@[\w-]+\.(com|net|org)\z/
->>>>>>> f3e9ede (Callback extension exercise)
   }
 
   after_create_commit :send_welcome_mail
