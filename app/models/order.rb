5ccb99b9646c8778cabf4a016eec31d506a64f80
Rashika <agarwalrashika00@gmail.com>
2023-04-18 15:54:55 +0530

    query interface updated

--- app/models/order.rb
+++ app/models/order.rb
@@ -9,6 +9,10 @@ class Order < ApplicationRecord
   validates :name, :address, :email, presence: true
   validates :pay_type, inclusion: pay_types.keys
 
+  scope :by_date, -> (from = Time.current.beginning_of_day, to = Time.current.end_of_day) {
+    where(created_at: from..to)
+  }
+
   def add_line_items_from_cart(cart)
     cart.line_items.each do |item|
       item.cart_id = nil
